3336bec5db7e1bbe9f17362da8450b4fc0df7103
Hyeonmin Seon <hmsun.es@esgroup.net>
2022-10-31 19:03:36 +0900

    불필요 파일 정리

---
+++ 0-module2.js
@@ -0,0 +1,14 @@
+/**
+ * TODO: 
+ * combineWithComma 함수를 실행하는 모듈을 작성하라. 모듈의 조건은 다음과 같다
+ * 
+ * Lodash 라이브러리를 찾아서 이 프로젝트에 추가하고
+ * 공식 사이트의 문서를 참조하여 모듈을 추가한 뒤
+ * 해당 함수가 온전히 작동하는 모듈 프로그램을 작성하라
+ * 
+ */
+
+// TODO: import module
+
+console.log(combineWithComma([1,2,3,4]));   // expected '1,2,3,4'
+console.log(combineWithComma(['abd', '123']));  // expected 'abd,123'
